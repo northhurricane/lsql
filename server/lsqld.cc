49573c9f2fee71708e45b623481b8ffa7ad51a52
jiangyx <jiangyx@ctrip.com>
2014-09-16 17:56:02 +0800

    session/task etc

---
+++ server/lsqld.cc
@@ -0,0 +1,6 @@
+#include "lsqld.h"
+
+int lsqld_main(int argc, char *argv)
+{
+  return 0;
+}
---
+++ server/task.h
@@ -0,0 +1,12 @@
+#ifndef _TASK_H
+#define _TASK_H
+
+class SESSION;
+
+class TASK
+{
+public :
+  SESSION *session;
+};
+
+#endif //_TASK_H
