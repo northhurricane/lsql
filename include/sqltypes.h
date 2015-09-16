2471af78ae057e108bac7f353f04f9b508a7851d
jiangyx <jiangyx@ctrip.com>
2015-09-16 17:32:54 +0800

    na

--- include/sqltypes.h
+++ include/sqltypes.h
@@ -1,12 +1,16 @@
 #ifndef LSQL_INCLUDE_SQLTYPE_H_
 #define LSQL_INCLUDE_SQLTYPE_H_
 
+#include "ltypedef.h"
+
 typedef int16_t sqltype_t;
 
 #define LSQL_TYPE_INVALID   0
 #define LSQL_TYPE_CHAR      1   //定长字符类型，内部存储为变长
 #define LSQL_TYPE_INT       2   //4字节长度整形
 #define LSQL_TYPE_VARCHAR   3   //变长字符类型
+
+#define LSQL_TYPE_INT_STORAGE_SIZE (
 
 #define LSQL_TYPE_INT_PREC  10
 #define LSQL_TYPE_INT_SCALE 0
@@ -29,5 +33,19 @@ inline bool sqltype_is_storage_fix(sqltype_t type)
     return false;
   }
 }
+
+inline uint16_t sqltype_storage_fix_size(sqltype_t type)
+{
+  switch (type)
+  {
+  case LSQL_TYPE_INT:
+    return LINT32_SIZE;
+
+  defaults :
+    //其他非固定存储的类型为0
+    return 0;
+  }
+}
+
 
 #endif //LSQL_INCLUDE_SQLTYPE_H_
--- sql/column.h
+++ sql/column.h
@@ -3,6 +3,7 @@
 
 #include "lsql.h"
 #include "lendian.h"
+#include "ltypedef.h"
 
 using namespace std;
 
@@ -16,6 +17,15 @@ struct coldef_struct
 typedef struct coldef_struct coldef_t;
 #define COLDEF_SIZE (sizeof(coldef_t[1]))
 
+#define COLDEF_NET_HEAD (0)
+#define COLDEF_NET_TYPE (COLDEF_NET_HEAD)
+#define COLDEF_NET_PREC (COLDEF_NETTYPE + LINT16_SIZE)
+#define COLDEF_NET_SCALE (COLDEF_NET_PREC + LINT16_SIZE)
+#define COLDEF_NET_NULLABLE (COLDEF_NET_SCALE + LINT16_SIZE)
+#define COLDEF_NET_TAIL (COLDEF_NET_NULLABLE + LINT8_SIZE)
+
+#define COLDEF_NET_SIZE COLDEF_NET_TAIL
+
 /*
 网络格式和结构之间的转换，n表示net
 */
@@ -26,8 +36,20 @@ typedef struct coldef_struct coldef_t;
 inline uint16_t
 coldef2n(coldef_t def, void *buffer, int size)
 {
+  if (size < COLDEF_NET_SIZE)
+    return 0;
+
   char *buffer_inner = (char*)buffer;
-  return 0;
+  lendian_write_uint16(buffer_inner, def.type);
+  buffer_inner += LINT16_SIZE;
+  lendian_write_uint16(buffer_inner, def.precision);
+  buffer_inner += LINT16_SIZE;
+  lendian_write_uint16(buffer_inner, def.scale);
+  buffer_inner += LINT16_SIZE;
+  lendian_write_uint8(buffer_inner, def.nullable);
+  buffer_inner += LINT8_SIZE;
+
+  return COLDEF_NET_SIZE;
 }
 
 /*
@@ -37,7 +59,18 @@ coldef2n(coldef_t def, void *buffer, int size)
 inline uint16_t
 n2coldef(coldef_t *def, void *buffer)
 {
-  return 0;
+  char *buffer_inner = (char*)buffer;
+
+  def->type = lendian_read_uint16(buffer_inner);
+  buffer_inner += LINT16_SIZE;
+  def->precession = lendian_read_uint16(buffer_inner);
+  buffer_inner += LINT16_SIZE;
+  def->scale = lendian_read_uint16(buffer_inner);
+  buffer_inner += LINT16_SIZE;
+  def->nullable = lendian_read_uint8(buffer_inner);
+  buffer_inner += LINT8_SIZE;
+
+  return COLDEF_NET_SIZE;
 }
 
 /*
@@ -45,14 +78,24 @@ n2coldef(coldef_t *def, void *buffer)
 */
 struct columns_def_struct
 {
+  /*增加列定义*/
   void add_coldef(coldef_t coldef)
   {
     if (sqltype_is_storage_fix(coldef.type))
     {
-      uint16_t storage_size = 0;
+      uint16_t storage_size = sqltype_storage_fix_size(coldef.type);
       fix_storage_size += storage_size;
     }
     columns.push_back(coldef);
+  }
+
+  /*存储null flag所占用的空间*/
+  uint16_t columns_nullflag_storage_space()
+  {
+    uint32_t column_number = columns.size();
+    if (column_number == 0)
+      return 0;
+    return column_number % * + 1;
   }
 
   uint16_t fix_storage_size; //固定长度字段所占的存储空间
