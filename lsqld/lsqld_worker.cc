a405c20e53980c40c203ce1c71dedf8da7512299
jiangyx <jiangyx@ctrip.com>
2017-11-03 17:48:12 +0800

    na

--- lsqld/lsqld_worker.cc
+++ lsqld/lsqld_worker.cc
@@ -3,6 +3,7 @@
 
 #include "LConn.h"
 #include "lsemaphore.h"
+#include "lmutex.h"
 
 using namespace std;
 
@@ -10,6 +11,7 @@ struct msg_task_queue_struct
 {
   list<LConn*> tasks;
   lsemaphore_t sem;
+  lmutex_t mutex;
 };
 typedef struct msg_task_queue_struct msg_task_queue_t;
 msg_task_queue_t msg_tasks;
@@ -17,16 +19,22 @@ msg_task_queue_t msg_tasks;
 void
 worker_msg_task_enqueue(LConn *conn)
 {
+  lmutex_lock(&msg_tasks.mutex);
   msg_tasks.tasks.push_back(conn);
+  lmutex_unlock(&msg_tasks.mutex);
+
+  lsemaphore_post(&msg_tasks.sem);
 }
 
 LConn*
 worker_msg_task_dequeue()
 {
+  lsemaphore_wait(&msg_tasks.sem);
+  lmutex_lock(&msg_tasks.mutex);
   LConn *conn = msg_tasks.tasks.front();
-  if (conn == NULL)
-    return NULL;
-  msg_tasks.tasks.pop_front();
+  if (conn != NULL)
+    msg_tasks.tasks.pop_front();
+  lmutex_unlock(&msg_tasks.mutex);
   return conn;
 }
 
