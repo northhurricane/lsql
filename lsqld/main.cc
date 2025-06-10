18650567439f2e2fb8ca7abb21c7c7fac93d1017
yuxianjiang <yuxianjiang@tencent.com>
2025-06-10 09:53:20 +0800

    进行lsqld的模块设计说明

--- lsqld/main.cc
+++ lsqld/main.cc
@@ -1,7 +1,10 @@
 extern int lsqld_main(int argc, char *argv[]);
 
+using namespace lsqld;
+
 //just make it easy to locate the entry for new guys
 int main(int argc, char *argv[])
 {
   return lsqld_main(argc, argv);
 }
+
